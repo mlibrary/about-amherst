0eedb8b41749a79e35d438a43450cce9dfb105fa
Seth A. Johnson <sethajoh@umich.edu>
2020-07-17 11:08:35 -0400

    amherst csm config

--- src/cms/cms.js
+++ src/cms/cms.js
@@ -12,10 +12,10 @@ if (process.env.BRANCH === "preview") {
     config: {
       backend: {
         name: 'gitlab',
-        repo: 'michigan-publishing/about-leverpress',
+        repo: 'michigan-publishing/about-amherstpress',
         branch: 'preview',
         auth_type: 'implicit',
-        app_id: '1ad010fc364de8a6303468218779296589f4f424dd3284e42bf954cad431395b',
+        app_id: '',
         api_root: 'https://gitlab.umich.edu/api/v4',
         base_url: 'https://gitlab.umich.edu',
         auth_endpoint: 'oauth/authorize'
@@ -27,10 +27,10 @@ if (process.env.BRANCH === "preview") {
     config: {
       backend: {
         name: 'gitlab',
-        repo: 'michigan-publishing/about-leverpress',
+        repo: 'michigan-publishing/about-amherstpress',
         branch: 'master',
         auth_type: 'implicit',
-        app_id: '03d3fe1c9a36d44c83ea5d1f0382ac41333bd88052587631ae9fa15ce6b15123',
+        app_id: '',
         api_root: 'https://gitlab.umich.edu/api/v4',
         base_url: 'https://gitlab.umich.edu',
         auth_endpoint: 'oauth/authorize'
