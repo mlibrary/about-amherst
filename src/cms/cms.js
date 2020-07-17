6a978a67d343963fe684ac18f8926bbb52ebbc2b
Seth A. Johnson <sethajoh@umich.edu>
2020-07-17 11:29:33 -0400

    config netlify cms

--- src/cms/cms.js
+++ src/cms/cms.js
@@ -30,7 +30,7 @@ if (process.env.BRANCH === "preview") {
         repo: 'michigan-publishing/about-amherstpress',
         branch: 'master',
         auth_type: 'implicit',
-        app_id: '',
+        app_id: 'ce8af269578025e622e241b12a09bfe4890a273447080f13e59a1590f1b882e0',
         api_root: 'https://gitlab.umich.edu/api/v4',
         base_url: 'https://gitlab.umich.edu',
         auth_endpoint: 'oauth/authorize'
