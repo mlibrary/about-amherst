cc547984f66a4aeae204851bc69a81f317585331
Seth A. Johnson <sethajoh@umich.edu>
2025-08-21 13:26:20 -0400

    HELIO-4960 fix catalogImage

--- src/components/catalog.js
+++ src/components/catalog.js
@@ -1,11 +1,11 @@
 import React from "react"
-import Img from "gatsby-image"
 
 const Catalog = ({catalog}) => {
+  const catalogImage = catalog.catalogImage.childImageSharp ? catalog.catalogImage.childImageSharp.fluid.src : catalog.catalogImage
   return (
     <div className="catalog-wrapper">
       <h2 className="sr-only">{catalog.heading}</h2>
-      <figure><img src={catalog.catalogImage} alt="Catalog Cover" className="catalog-image"/></figure>
+      <figure><img src={catalogImage} alt="Catalog Cover" className="catalog-image"/></figure>
       <div dangerouslySetInnerHTML={{ __html: catalog.catalogPdf }} />
     </div>
   )
